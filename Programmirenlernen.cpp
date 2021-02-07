cfd2bd7d2ef429cfc9495c818d1248dade9b3a40
Guenther-Engelbert <78657704+Guenther-Engelbert@users.noreply.github.com>
2021-02-07 17:25:43 +0100

    Add files via upload

---
+++ Programmirenlernen.cpp
@@ -0,0 +1,101 @@
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::cout << "Wie alt Bist Du? ";
+    int alter;
+    cin >> alter;
+    if (alter < 18) {
+        cout << "sie sind Minderjährig.";
+        return 0;
+
+    }
+    cout << "Sie sind Volljährig.";
+    string name;
+    cout << " Wie heißen Sie? ";
+    cin >> name;
+    cout << "Hallo " << name;
+
+
+
+    cout << " Willkommen in der Lotterie. ";
+    int  vierte_Zahl;
+    cout << "Bitte geben Sie eine Zahl zwischen 1 und 9 ein: ";
+    cin >> vierte_Zahl;
+
+    while (vierte_Zahl > 9) {
+        cout << "Die Zahl ist gößer als 9";
+        cin >> vierte_Zahl;
+    }
+
+    while (vierte_Zahl == erste_Zahl && vierte_Zahl == zweite_Zahl && vierte_Zahl == dritte_Zahl) {
+        cout << "Die Zahl wurde xschon eingeben, versuchen Sie es erneut" << endl;
+        cin >> vierte_Zahl;
+
+    }
+    int  vierte_Zahl;
+    cout << "Bitte geben Sie eine Zahl zwischen 1 und 9 ein: ";
+    cin >> vierte_Zahl;
+
+    while (vierte_Zahl > 9) {
+        cout << "Die Zahl ist gößer als 9";
+        cin >> vierte_Zahl;
+    }
+
+    while (vierte_Zahl == erste_Zahl && vierte_Zahl == zweite_Zahl && vierte_Zahl == dritte_Zahl) {
+        cout << "Die Zahl wurde xschon eingeben, versuchen Sie es erneut" << endl;
+        cin >> vierte_Zahl;
+
+    };
+    }
+    int  vierte_Zahl;
+    cout << "Bitte geben Sie eine Zahl zwischen 1 und 9 ein: ";
+    cin >> vierte_Zahl;
+
+    while (vierte_Zahl > 9) {
+        cout << "Die Zahl ist gößer als 9";
+        cin >> vierte_Zahl;
+    }
+
+    while (vierte_Zahl == erste_Zahl && vierte_Zahl == zweite_Zahl && vierte_Zahl == dritte_Zahl) {
+        cout << "Die Zahl wurde xschon eingeben, versuchen Sie es erneut" << endl;
+        cin >> vierte_Zahl;
+
+    }
+    }
+    int  vierte_Zahl;
+    cout << "Bitte geben Sie eine Zahl zwischen 1 und 9 ein: ";
+    cin >> vierte_Zahl;
+
+    while (vierte_Zahl > 9) {
+        cout << "Die Zahl ist gößer als 9";
+        cin >> vierte_Zahl;
+    }
+
+    while (vierte_Zahl == erste_Zahl && vierte_Zahl == zweite_Zahl && vierte_Zahl == dritte_Zahl) {
+        cout << "Die Zahl wurde xschon eingeben, versuchen Sie es erneut" << endl;;
+        cin >> vierte_Zahl;
+
+    }
+
+    
+
+    
+
+    int Lerste_Zahl = 4;
+    int Lzweite_Zahl = 5;
+    int Ldritte_Zahl = 6;
+    int Lvierte_Zahl = 1;
+
+    if (erste_Zahl == Lerste_Zahl || erste_Zahl== Lzweite_Zahl || erste_Zahl == Ldritte_Zahl || erste_Zahl == Lvierte_Zahl && zweite_Zahl == Lerste_Zahl || zweite_Zahl == Lzweite_Zahl || zweite_Zahl == Ldritte_Zahl || zweite_Zahl == Lvierte_Zahl && dritte_Zahl == Lerste_Zahl || dritte_Zahl == Lzweite_Zahl || dritte_Zahl == Ldritte_Zahl || dritte_Zahl == Lvierte_Zahl && vierte_Zahl == Lerste_Zahl || vierte_Zahl == Lzweite_Zahl || vierte_Zahl == Ldritte_Zahl || vierte_Zahl == Lvierte_Zahl) {
+        cout << "Herzlichen Glückwunsch " << name << " Sie haben die Lotterie Gewonnen.";
+    }
+
+    else cout << "Sie haben Verloren. Die Zahlen waren " << Lerste_Zahl<< Lzweite_Zahl<< Ldritte_Zahl<< Lvierte_Zahl;
+
+  
+
+
